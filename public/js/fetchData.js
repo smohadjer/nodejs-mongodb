15e625e5fb66467f43b211fb448d3469d701073c
Saeid Mohadjer <saeid@fastmail.fm>
2023-03-28 10:41:12 +0200

    adds ts

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,17 +1,18 @@
 export const fetchData = (id, callback) => {
-  fetch('/api/fetch.js', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({id: id})
-  })
-  .then((response) => response.json())
-  .then((json) => {
-      callback(json);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+    fetch('/api/fetch.js', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: id })
+    })
+        .then((response) => response.json())
+        .then((json) => {
+        callback(json);
+    })
+        .catch((error) => {
+        console.log(error);
+    });
 };
+//# sourceMappingURL=fetchData.js.map
---
+++ public/ts/fetchData.ts
@@ -0,0 +1,17 @@
+export const fetchData = (id, callback) => {
+  fetch('/api/fetch.js', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({id: id})
+  })
+  .then((response) => response.json())
+  .then((json) => {
+      callback(json);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+};
